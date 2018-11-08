627faf7ab5dfe6d26d515e6b42b9a8205af152dc
Ben R. Cowin <ben.cowin@outlook.com>
2018-11-08 16:48:55 +0000

    First commit! - Two small image related plugins to aid in achieving perfect alignment on responsive websites.

---
+++ imagery.js
@@ -0,0 +1,51 @@
+(function ( $ ) { 
+    $.fn.aspectRatio = function(options) {
+        var settings = $.extend({
+            heightRoundPercent: 1
+        }, options );
+
+        var roundTo = settings.heightRoundPercent;
+        var w = $(this).width();
+        var h = $(this).height();       
+
+        var a = h / w;
+
+        // e.g. a = .66 , a = 2, a = 1.5
+        var b = (Math.round((a * 100) / Number(roundTo)) * roundTo) / 100;
+
+        var ratio = [1, b];
+    
+        return ratio;
+    }; 
+}( jQuery ));
+
+(function ( $ ) { 
+    $.fn.makeImageBackground = function(options) {
+        var settings = $.extend({
+            heightPercent: 100
+        }, options );
+
+        var img = $(this);
+        var imgUrl = img.prop('src');
+        
+        var wrapper = img.wrap("<div class=\"background-image-wrap\"></div>").closest(".background-image-wrap");
+    
+        wrapper.css({
+            backgroundPosition: "center center",
+            backgroundSize: "cover",
+            backgroundRepeat: "no-repeat",
+            backgroundImage: "url(" + imgUrl + ")",
+            width: "100%",
+            position: "relative",
+            paddingBottom: String(settings.heightPercent) + "%"
+        });
+
+        wrapper.addClass("background-image-vertical-ratio-percentage-" + settings.heightPercent + "-applied");
+
+        img.css({
+            display: "none"
+        });
+
+        return img;
+    }; 
+}( jQuery ));
